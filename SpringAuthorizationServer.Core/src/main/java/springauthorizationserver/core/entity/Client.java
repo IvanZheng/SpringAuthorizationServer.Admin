d07c113bf39a392ad0c3d597591fd223bd9dbd7b
Ivan Zheng <yvonne_7_7@hotmail.com>
2023-11-13 22:02:29 +0800

    use Instant.now instead of null for clientIdIssuedAt

--- SpringAuthorizationServer.Core/src/main/java/springauthorizationserver/core/entity/Client.java
+++ SpringAuthorizationServer.Core/src/main/java/springauthorizationserver/core/entity/Client.java
@@ -55,7 +55,7 @@ public class Client {
     }
 
     public void setClientIdIssuedAt(Instant clientIdIssuedAt) {
-        this.clientIdIssuedAt = clientIdIssuedAt;
+        this.clientIdIssuedAt = clientIdIssuedAt != null ? clientIdIssuedAt : Instant.now();
     }
 
     public String getClientSecret() {
